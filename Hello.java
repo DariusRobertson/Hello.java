8153a0e34f9b4c1dd3238f6e0cad76c57ff0e9be
dariusamaan1 <54364572+dariusamaan1@users.noreply.github.com>
2019-08-21 10:22:52 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*******************************************************
+* Hello.Java 
+*********************************************************
+* First Lab 
+*________________________________________________________
+* Darius Robertson 
+* 08/21
+* CMSC 255 003
+*********************************************************/
+
+public class Hello { 
+ public static void printHeading() { 
+  System.out.println("Darius Robertson");
+  System.out.println("Hello.java");
+  System.out.println("first lab");
+  System.out.println("version 8"); 
+  System.out.println("CMSC 255 003");
+  System.out.println();
+ } 
+ 
+ public static void main(String [] args) {
+  printHeading();
+  System.out.println("Hello"); 
+ }
+}
